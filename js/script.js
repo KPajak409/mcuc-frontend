78a14ab43668294eb8c0c678168e83a43e4ec1c3
Gloomy-stack <karl.yotl@gmail.com>
2022-01-17 20:54:13 +0100

    Responsive for mobile

--- js/script.js
+++ js/script.js
@@ -9,3 +9,8 @@ window.onload = () => {
     }
   }
 };
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const nav = document.getElementsByTagName("nav")[0];
+
+toggleButton.addEventListener("click", () => nav.classList.toggle("active"));
